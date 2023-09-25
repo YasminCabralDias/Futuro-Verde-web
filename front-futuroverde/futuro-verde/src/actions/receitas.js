96394e3ab172946a48f6acbdf056c3981e67ee58
YasminCabralDias <rm95800@fiap.com.br>
2023-09-25 15:47:29 -0300

    att(update e delete)

--- front-futuroverde/futuro-verde/src/actions/receitas.js
+++ front-futuroverde/futuro-verde/src/actions/receitas.js
@@ -2,8 +2,10 @@
 
 import { revalidatePath } from "next/cache"
 
+const url = process.env.NEXT_PUBLIC_BASE_URL + "/receitas"
+
 export async function create(formData) {
-    const url = "http://localhost:8080/api/receitas"
+    
 
     const options = {
         method: "POST",
@@ -22,3 +24,45 @@ export async function create(formData) {
     revalidatePath("/receitas")
     return {ok: "success"}
 } 
+
+
+export async function destroy(id){  
+    const deleteUrl = url + "/" + id
+
+    const options = {
+        method: "DELETE"
+    }
+
+    const resp = await fetch(deleteUrl, options)
+
+    if (resp.status !== 204) return {error: "Erro ao apagar conta. " + resp.status}
+
+    revalidatePath("/contas")
+
+}
+
+export async function update(receita){
+    const updateURL = url + "/" + receita.id
+
+    const options = {
+        method: "PUT",
+        body: JSON.stringify(receita),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    const resp = await fetch(updateURL, options)
+
+    if (resp.status !== 200) return {error: "Erro ao atualizar a receita. " + resp.status}
+
+    revalidatePath("/")
+}
+
+export async function getReceita(id){
+    const getUrl = url + "/" + id
+    const resp = await fetch(getUrl)
+    if (resp.status !== 200) return {error: "Erro ao buscar dados da receita. " + resp.status}
+    const json = await resp.json()
+    return json
+}
