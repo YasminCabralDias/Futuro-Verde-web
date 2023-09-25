55bc06b1f24de10f3947778980e292c152898ef1
YasminCabralDias <rm95800@fiap.com.br>
2023-09-25 15:53:29 -0300

    fix

--- front-futuroverde/futuro-verde/src/app/receitas/page.js
+++ front-futuroverde/futuro-verde/src/app/receitas/page.js
@@ -2,12 +2,6 @@
 import DataRow from "./DataRow";
 import Button from "@/components/Button";
 
-async function getReceitas(){
-  const url = "http://localhost:8080/api/receitas"
-  const resp = await fetch(url, { next: { revalidate: 3600 } })
-  if (!resp.ok) throw new Error("Não pode carregar os dados")
-  return resp.json()
-}
 
 export default async function Home() {
   const data = await getReceitas()
