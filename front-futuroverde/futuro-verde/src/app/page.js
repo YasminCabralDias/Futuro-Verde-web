d64b51bba1b0e02bf0358236d8647a25ed17013d
YasminCabralDias <rm95800@fiap.com.br>
2023-10-02 18:59:47 -0300

    att

--- front-futuroverde/futuro-verde/src/app/page.js
+++ front-futuroverde/futuro-verde/src/app/page.js
@@ -1,4 +1,5 @@
 import Home from '/src/components/home.jsx'
+import NavBar from "@/components/NavBar";
 
 export default function Login() {
   return (
--- front-futuroverde/futuro-verde/src/app/receitas/page.js
+++ front-futuroverde/futuro-verde/src/app/receitas/page.js
@@ -2,29 +2,28 @@ import NavBar from "@/components/NavBar";
 import DataRow from "./DataRow";
 import Button from "@/components/Button";
 import { getReceitas } from "@/actions/receitas";
-
+import NavBar from "@/components/NavBar";
 
 export default async function Home() {
   const data = await getReceitas()
 
-return (
+  return (
     <>
       <NavBar active={"receitas"} />
 
-      <main className="bg-green-500 m-20 p-12 rounded-xl">
-
-      <div className="flex justify-between items-center">
-          <h2 className="text-2xl">Receitas</h2>
+      <main className="bg-slate-900 mt-20 m-auto max-w-lg p-12 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-bold">Receitas</h2>
           <Button href="/receitas/new">
-            Cadastrar
+            Adicionar receita
           </Button>
-      </div>
+        </div>
 
-        <div id="data">
-          {data.map(receita => <DataRow receita={receita} /> )}
+        <div id="data" className="text-slate-300 m-1">
+          {data.map(conta => <DataRow receita={receita} /> )}
         </div>
       </main>
     </>
- 
+
   )
 }
