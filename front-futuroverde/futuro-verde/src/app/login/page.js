0d9c5a04067edaa67526b35c434b8e934355ea2c
YasminCabralDias <rm95800@fiap.com.br>
2023-10-02 21:02:15 -0300

    att

--- front-futuroverde/futuro-verde/src/app/login/page.js
+++ front-futuroverde/futuro-verde/src/app/login/page.js
@@ -9,50 +9,35 @@ import { AuthContext } from "@/contexts/AuthContext";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
-export default function Login() {
-    return (
-      <>
-      <Header/>
-  
-          <div className="bg-scroll" style={{backgroundImage: `url('https://cdn.discordapp.com/attachments/1021132314446672003/1143281878762258463/image.png')`, height: "900px", backgroundSize: "cover"}}>
-          <form class="flex flex-col justify-center items-center">
-                <div class="md:flex md:items-center mb-6">
-                  <div class="md:w-1/3">
-                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-                      Email
-                    </label>
-                  </div>
-                  <div class="md:w-2/3">
-                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:bg-white" id="inline-full-name" type="text" value="Jane Doe"/>
-                  </div>
-                </div>
-                <div class="md:flex md:items-center mb-6">
-                  <div class="md:w-1/3">
-                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
-                      Senha
-                    </label>
-                  </div>
-                  <div class="md:w-2/3">
-                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:bg-white" id="inline-password" type="password" placeholder="******************"/>
-                  </div>
-                </div>
-                <div class="md:flex md:items-center mb-6">
-                  <div class="md:w-1/3"></div>
-                  <label class="md:w-2/3 block text-gray-500 font-bold">
-  
-                  </label>
-                </div>
-                <div class="md:flex md:items-center">
-                  <div class="md:w-1/3"></div>
-                  <div class="md:w-2/3">
-                    <button class="shadow bg-white hover:bg-whitefocus:shadow-outline focus:outline-none text-black font-bold py-2 px-4 rounded" type="button">
-                      Entrar
-                    </button>
-                  </div>
-                </div>
-              </form>
-              </div>
+export default function LoginPage(){
+    const { register, handleSubmit } = useForm()
+    const { login } = useContext(AuthContext)
+    const { push } = useRouter()
 
-    </>
-  )
-}
+    async function onSubmit(data){
+        const resp = await login(data)
+        if (resp?.error) {
+            toast.error(resp?.error)
+        }else{
+            push("/")
+        }
+
+    }
+
+    return(
+        <div className="flex h-screen">
+            <aside className="hidden lg:flex">
+                <Image src={loginimage} className="h-full w-full object-cover"/>
+            </aside>
+
+            <main className="flex flex-col items-center justify-center w-full">
+                <h1 className="text-5xl font-bold">FuturoVerde</h1>
+                <form onSubmit={handleSubmit(onSubmit)} className=" flex flex-col gap-2 items-start mt-20">
+                    <InputText register={register} name="email" label="email" />
+                    <InputText register={register} name="senha" label="senha" type="password" />
+                    <Button element="button">entrar</Button>
+                </form>
+            </main>
+        </div>
+          )
+        }
